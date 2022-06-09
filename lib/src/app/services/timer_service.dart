ea6cecfe05c3ddaf0658444cdd218e7c0b97435b
Francis Nduba Numbi <francisnnumbi@yandex.com>
2022-06-09 23:42:50 +0200

    improving layout and performance

--- lib/src/app/services/timer_service.dart
+++ lib/src/app/services/timer_service.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:smirl_timer/src/app/models/event_model.dart';
@@ -53,9 +54,58 @@ class TimerService extends GetxService {
   }
 
   deleteEvent(EventModel event) {
-    List<dynamic> evts = GetStorage().read('events');
+    Get.defaultDialog(
+      title: "Supprimer l'événement ?",
+      content: const Text("Êtes-vous sûr de vouloir supprimer cet événement ?"),
+      actions: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.grey,
+          ),
+          child: const Text('Cancel'),
+          onPressed: () {
+            Get.back();
+          },
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.red,
+          ),
+          onPressed: () {
+            List<dynamic> evts = GetStorage().read('events');
+            int idx = evts.indexWhere((element) {
+              return element['timeStamp'] == event.timeStamp;
+            });
+            evts.removeWhere((element) {
+              return element['timeStamp'] == event.timeStamp;
+            });
+            if (evts.isEmpty) {
+              currentEvent.value = null;
+            } else {
+              currentEvent.value = EventModel.fromJson(evts[0]);
+            }
+
+            /*else if (events.value.length - 1 < idx) {
+              currentEvent.value =
+                  EventModel.fromJson(evts[events.value.length - 1]);
+            }*/
+            /*else {
+              if (idx == 0) {
+                currentEvent.value = EventModel.fromJson(evts[0]);
+              } else {
+                currentEvent.value = EventModel.fromJson(evts[idx - 1]);
+              }
+            }*/
+            GetStorage().write('events', evts);
+            Get.back();
+          },
+          child: const Text('Supprimer'),
+        ),
+      ],
+    );
+    /*  List<dynamic> evts = GetStorage().read('events');
     evts.removeWhere((element) => element['timeStamp'] == event.timeStamp);
-    GetStorage().write('events', evts);
+    GetStorage().write('events', evts);*/
   }
 
   selectEvent(EventModel event) {
